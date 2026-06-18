a0ef62edb66b18475181a647473ecaebdb048415
Arvind Kumar <arvindkrbscc2006@gmail.com>
2026-06-18 15:25:23 +0530

    Update auth-router.js

--- Backend/Router/auth-router.js
+++ Backend/Router/auth-router.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const authcontrollers= require("../controllers/auth-controller")
+const authcontrollers= require("../Controllers/auth-controller")
 const { signupSchema, loginSchema } = require("../validators/auth-validator");
-const validate = require("../middlewares/validate-middleware");
-const authmiddleware = require("../middlewares/auth-middleware");
+const validate = require("../Middlewares/validate-middleware");
+const authmiddleware = require("../Middlewares/auth-middleware");
 
 router.route("/").get(authcontrollers.home);
 router.route("/register").post(validate(signupSchema),  authcontrollers.register);
